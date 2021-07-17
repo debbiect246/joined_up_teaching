2d6b177896312135eced0471be7e5274ac818b28
Deborah Thompson <deb246@btinternet.com>
2021-07-17 23:31:50 +0100

    navbar fixed

--- app.py
+++ app.py
@@ -8,34 +8,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Home")
 
 #url for psychology of teaching page
+#adding title to render template puts title of page on screen
  
 @app.route('/psychteaching/')
 def psychteaching():
-    return render_template('psychteaching.html')
+    return render_template('psychteaching.html', title="Psychology of Teaching")
     
 #url for stress page
 
 @app.route('/stress/')
 def stress():
-    return render_template('stress.html')
+    return render_template('stress.html', title = "Dealing with Stress")
 
 #url for admnin page
 @app.route('/admin/')
 def admin():
-    return render_template('/admin/')
+    return render_template('admin.html', title = "Managing Admin")
 
 #url for contact page
 @app.route('/contact/')
 def contact():
-    return render_template('/contact/')
+    return render_template('contact.html', title= "Contact Me")
 
 #url for blog page
 @app.route('/blog/')
 def blog():
-    return render_template('/blog/')
+    return render_template('blog.html', title = "Blog")
 
 if __name__ =='__ main__':
     app.run
